1be7380d99485eaca4bf3e07635cda307fa4a5e9
Edgardo Yoliani <edrayoca@guarapolabs.com>
2022-03-10 17:21:53 -0500

    commit

--- src/components/timeslot/ModalDayTimeslots.js
+++ src/components/timeslot/ModalDayTimeslots.js
@@ -8,21 +8,21 @@ import moment from 'moment';
 import Timeslot from '../Timeslosts';
 const ModalDayTimeslots = ({ timeslot, date, show, handleClose }) => {
   const now = new Date();
-
+  console.log(date);
   return (
     <div>
       <Modal show={show} fullscreen={true} onHide={() => handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>
+            {timeslot.map((item, index) =>
+              item.date === date ? (
+                <Timeslot timeslot={item} index={index} />
+              ) : null
+            )}
+
             <Button onClick={handleClose}>Go back</Button>
           </Modal.Title>
         </Modal.Header>
-        {timeslot.map((item, index) => {
-          if (item.date === date) {
-            <Timeslot timeslot={item} index={index} />;
-          }
-        })}
-
         <Modal.Body></Modal.Body>
       </Modal>
     </div>
